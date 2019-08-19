dad26ccf94fb61471741f213caa89285e148c710
Evan Taylor <taylorevan113@gmail.com>
2019-08-19 08:46:48 -0500

    Completed UsersView and OrdersView

---
+++ GUI/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WVA_Connect_CSI.Models
+{
+    class Order
+    {
+        public string WvaStoreID { get; set; }
+        public string OrderName { get; set; }
+        public string CreatedDate { get; set; }
+        public string ShipToPatient { get; set; }
+        public string PoNumber { get; set; }
+        public string OrderedBy { get; set; }
+        public int Quantity { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ GUI/Views/AdminMainView.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WVA_Connect_CSI.Views
+{
+    /// <summary>
+    /// Interaction logic for AdminMainView.xaml
+    /// </summary>
+    public partial class AdminMainView : UserControl
+    {
+        private string UserRole;
+
+        private string GetUserRole()
+        {
+            return UserRole;
+        }
+
+        private void SetUserRole(string value)
+        {
+            UserRole = value;
+        }
+
+        public AdminMainView(string role)
+        {
+            InitializeComponent();
+            ResizeView();
+            SetRole(role);
+            SetView(role);
+        }
+
+        private void ResizeView()
+        {
+            foreach (Window window in Application.Current.Windows)
+                if (window.GetType() == typeof(MainWindow))
+                {
+                    (window as MainWindow).ResizeMode = ResizeMode.CanResize;
+                    (window as MainWindow).Height = 450;
+                    (window as MainWindow).Width = 800;
+                }
+        }
+        private void SetRole(string role)
+        {
+            UserRole = role;
+        }
+
+        private void SetView(string view)
+        {
+            switch (view)
+            {
+                case "ordersView":
+                    AdminMainViewContentControl.Content = new OrdersView();
+                    break;
+                case "orderDetailsView":
+                    AdminMainViewContentControl.Content = new OrderDetailsView();
+                    break;
+                case "usersView":
+                    AdminMainViewContentControl.Content = new UsersView();
+                    break;
+                default:
+                    
+                    break;
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Window window in Application.Current.Windows)
+                if (window.GetType() == typeof(MainWindow))
+                    (window as MainWindow).MainContentControl.DataContext = new MainView();
+        }
+
+        private void OrdersButton_Click(object sender, RoutedEventArgs e)
+        {
+            SetView("ordersView");
+        }
+
+        private void UsersButton_Click(object sender, RoutedEventArgs e)
+        {
+            SetView("usersView");
+        }
+    }
+}
