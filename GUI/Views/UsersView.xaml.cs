670fd4e607cf84b08863a5a692d3596655d3dc12
Evan Taylor <taylorevan113@gmail.com>
2019-09-19 10:57:44 -0500

    IT Admins can quickly search for users in the database

--- GUI/Views/UsersView.xaml.cs
+++ GUI/Views/UsersView.xaml.cs
@@ -60,76 +60,6 @@ namespace WVA_Connect_CSI.Views
             UsersDataGridContextMenu.Items.Add(menuItem);
         }
 
-        //
-        // Import users from a CSV file
-        //
-
-        private void ImportUsersButton_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                ActionLogger.Log(GetType().FullName + nameof(ImportUsersButton_Click), userRole, "<Import_Users_Start>");
-                usersViewModel.ImportUsers(userRole);
-                RefreshGrid();
-            }
-            catch (FileNotFoundException ex)
-            {
-                MessageBox.Show($"Cannot find path to file '{ex.Message}'", "File Not Found", MessageBoxButton.OK);
-            }
-            catch (FileFormatException)
-            {
-               
-            }
-            catch (Exception ex)
-            {
-
-            }
-            finally
-            {
-                ActionLogger.Log(GetType().FullName + nameof(ImportUsersButton_Click), userRole, "<Import_Users_End>");
-            }
-        }
-
-        //
-        // Create a new user in the database
-        //
-
-        private void TryCreateUser()
-        {
-            try
-            {
-                ActionLogger.Log(GetType().FullName + nameof(TryCreateUser), userRole, $"<Creating_User UserName={UserNameTextBox.Text}, Email={EmailTextBox.Text}, RoleId={RoleTextBox.Text}>");
-
-                if (usersViewModel.UserNameExists(UserNameTextBox.Text))
-                {
-                    MessageBox.Show("This username exists already!", "", MessageBoxButton.OK);
-                }
-                else if (usersViewModel.EmailExists(EmailTextBox.Text))
-                {
-                    MessageBox.Show("This email exists already!", "", MessageBoxButton.OK);
-                }
-                else
-                {
-                    bool created = usersViewModel.CreateUser(UserNameTextBox.Text, Crypto.ConvertToHash(PasswordTextBox.Text), EmailTextBox.Text, RoleTextBox.SelectedIndex, 0);
-
-                    if (created)
-                    {
-                        MessageBox.Show("User Created!", "", MessageBoxButton.OK);
-                        UserNameTextBox.Text = "";
-                        PasswordTextBox.Text = "";
-                        EmailTextBox.Text = "";
-                        RoleTextBox.SelectedIndex = 0;
-                    }
-                    else
-                        MessageBox.Show("Error Creating User!", "", MessageBoxButton.OK);
-                }
-            }
-            catch (Exception ex)
-            {
-                Error.ReportOrLog(ex);
-            }
-        }
-
         private void RefreshGrid()
         {
             // Remove all items in grid
@@ -167,23 +97,39 @@ namespace WVA_Connect_CSI.Views
                 return true;
         }
 
-        private void DeleteUsers(List<User> usersToDelete)
+        //
+        // Import users from a CSV file
+        //
+
+        private void ImportUsersButton_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                // delete user from data base and remove it from the grid
-                foreach (User user in usersToDelete)
-                {
-                    ActionLogger.Log(GetType().FullName + nameof(UsersDataGridContextMenu_Click), userRole, $"<Delete_User> UserName={user.UserName}, Email={user.Email}, Role={user.RoleId}");
-                    database.DeleteUser(user.UserName);
-                    UsersDataGrid.Items.Remove(user);
-                }
+                ActionLogger.Log(GetType().FullName + nameof(ImportUsersButton_Click), userRole, "<Import_Users_Start>");
+                usersViewModel.ImportUsers(userRole);
+                RefreshGrid();
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show($"Cannot find path to file '{ex.Message}'", "File Not Found", MessageBoxButton.OK);
+            }
+            catch (FileFormatException)
+            {
+               
             }
             catch (Exception ex)
             {
-                Error.ReportOrLog(ex);
+
+            }
+            finally
+            {
+                ActionLogger.Log(GetType().FullName + nameof(ImportUsersButton_Click), userRole, "<Import_Users_End>");
             }
         }
+
+        //
+        // Create users
+        //
 
         private void AddUserButton_Click(object sender, RoutedEventArgs e)
         {
@@ -196,34 +142,45 @@ namespace WVA_Connect_CSI.Views
                 return;
         }
 
-        private void UserNameTextBox_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        private void TryCreateUser()
         {
-            UserNameTextBox.Focus();
-            UserNameTextBox.SelectAll();
+            try
+            {
+                ActionLogger.Log(GetType().FullName + nameof(TryCreateUser), userRole, $"<Creating_User UserName={UserNameTextBox.Text}, Email={EmailTextBox.Text}, RoleId={RoleTextBox.Text}>");
+
+                if (usersViewModel.UserNameExists(UserNameTextBox.Text))
+                {
+                    MessageBox.Show("This username exists already!", "", MessageBoxButton.OK);
+                }
+                else if (usersViewModel.EmailExists(EmailTextBox.Text))
+                {
+                    MessageBox.Show("This email exists already!", "", MessageBoxButton.OK);
+                }
+                else
+                {
+                    bool created = usersViewModel.CreateUser(UserNameTextBox.Text, Crypto.ConvertToHash(PasswordTextBox.Text), EmailTextBox.Text, RoleTextBox.SelectedIndex, 0);
+
+                    if (created)
+                    {
+                        MessageBox.Show("User Created!", "", MessageBoxButton.OK);
+                        UserNameTextBox.Text = "";
+                        PasswordTextBox.Text = "";
+                        EmailTextBox.Text = "";
+                        RoleTextBox.SelectedIndex = 0;
+                    }
+                    else
+                        MessageBox.Show("Error Creating User!", "", MessageBoxButton.OK);
+                }
+            }
+            catch (Exception ex)
+            {
+                Error.ReportOrLog(ex);
+            }
         }
 
-        private void PasswordTextBox_PreviewMouseUp(object sender, MouseButtonEventArgs e)
-        {
-            PasswordTextBox.Focus();
-            PasswordTextBox.SelectAll();
-        }
-
-        private void EmailTextBox_PreviewMouseUp(object sender, MouseButtonEventArgs e)
-        {
-            EmailTextBox.Focus();
-            EmailTextBox.SelectAll();
-        }
-
-        private void SearchUsersTextBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            SearchUsersTextBox.Text = "";
-        }
-
-        private void SearchUsersTextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (SearchUsersTextBox.Text == "")
-                SearchUsersTextBox.Text = "Search Users... ";
-        }
+        //
+        // Delete users
+        //
 
         private void UsersDataGridContextMenu_Click(object sender, RoutedEventArgs e)
         {
@@ -250,5 +207,76 @@ namespace WVA_Connect_CSI.Views
             }
         }
 
+        private void DeleteUsers(List<User> usersToDelete)
+        {
+            try
+            {
+                // delete user from data base and remove it from the grid
+                foreach (User user in usersToDelete)
+                {
+                    ActionLogger.Log(GetType().FullName + nameof(UsersDataGridContextMenu_Click), userRole, $"<Delete_User> UserName={user.UserName}, Email={user.Email}, Role={user.RoleId}");
+                    database.DeleteUser(user.UserName);
+                    UsersDataGrid.Items.Remove(user);
+                }
+            }
+            catch (Exception ex)
+            {
+                Error.ReportOrLog(ex);
+            }
+        }
+
+        //
+        // Search users
+        //
+
+        private void SearchUsersTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+          
+            if (UsersDataGrid?.Items?.Count != null && SearchUsersTextBox.Text != "Search Users...")
+            {
+                UsersDataGrid.Items.Clear();
+
+                var users = database.GetUsers();
+                users.RemoveAll(x => !x.UserName.ToLower().StartsWith(SearchUsersTextBox.Text));
+
+                foreach (User user in users)
+                    UsersDataGrid.Items.Add(user);
+
+                UsersDataGrid.Items.Refresh();
+            }
+        }
+
+        //
+        // Misc Events
+        //
+
+        private void UserNameTextBox_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            UserNameTextBox.Focus();
+            UserNameTextBox.SelectAll();
+        }
+
+        private void PasswordTextBox_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            PasswordTextBox.Focus();
+            PasswordTextBox.SelectAll();
+        }
+
+        private void EmailTextBox_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            EmailTextBox.Focus();
+            EmailTextBox.SelectAll();
+        }
+
+        private void SearchUsersTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (SearchUsersTextBox.Text == "")
+                SearchUsersTextBox.Text = "Search Users...";
+        }
+
+        private void SearchUsersTextBox_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            SearchUsersTextBox.Select(0, SearchUsersTextBox.Text.Length);
+        }
     }
 }
