340f2c4e368c6ae780c0bdbcba1776de96fffe82
Evan Taylor <taylorevan113@gmail.com>
2019-10-02 14:10:31 -0500

    ServiceHost tests completed

--- GUI.UnitTests/Services/ServiceHostTests.cs
+++ GUI.UnitTests/Services/ServiceHostTests.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using WVA_Connect_CSI.Services;
 using WVA_Connect_CSI.Utility.Files;
@@ -13,9 +15,37 @@ namespace GUI.UnitTests.Services
     class ServiceHostTests
     {
         [TestMethod]
-        public  void ServiceHost_Install_ServiceExistsTrue()
+        public  void ServiceHost_Install_IsInstalled_True()
         {
             ServiceHost.Install();
+
+            Thread.Sleep(2000); // Wait for system to install service
+
+            bool actualValue = ServiceHost.IsInstalled();
+            bool expectedValue = true;
+
+            Assert.AreEqual(actualValue, expectedValue);
+        }
+
+        [TestMethod]
+        public void ServiceHost_Uninstall_IsInstalled_False()
+        {
+            ServiceHost.Uninstall();
+
+            Thread.Sleep(2000); // Wait for system to remove service
+
+            bool actualValue = ServiceHost.IsInstalled();
+            bool expectedValue = false;
+
+            Assert.AreEqual(actualValue, expectedValue);
+        }
+
+        [TestMethod]
+        public void ServiceHost_Start_IsRunning_True()
+        {
+            ServiceHost.Start();
+
+            Thread.Sleep(2000); // Wait for system to spin up service
 
             bool actualValue = ServiceHost.IsRunning();
             bool expectedValue = true;
@@ -24,12 +54,40 @@ namespace GUI.UnitTests.Services
         }
 
         [TestMethod]
-        public void ServiceHost_Uninstall_ServiceExistFalse()
+        public void ServiceHost_Stop_IsRunning_False()
         {
-            ServiceHost.Install();
+            ServiceHost.Stop();
+
+            Thread.Sleep(2000); // Wait for system to terminate service
 
             bool actualValue = ServiceHost.IsRunning();
+            bool expectedValue = false;
+
+            Assert.AreEqual(actualValue, expectedValue);
+        }
+
+        [TestMethod]
+        public void ServiceHost_IsRunning_True()
+        {
+            ServiceHost.Start();
+
+            Thread.Sleep(2000); // Wait for system to start the service
+
+            bool actualValue = new ServiceController("WVA_Connect_CSI_Service").Status == ServiceControllerStatus.Running ? true : false;
             bool expectedValue = true;
+
+            Assert.AreEqual(actualValue, expectedValue);
+        }
+
+        [TestMethod]
+        public void ServiceHost_IsRunning_False()
+        {
+            ServiceHost.Stop();
+
+            Thread.Sleep(2000); // Wait for system to start the service
+
+            bool actualValue = new ServiceController("WVA_Connect_CSI_Service").Status == ServiceControllerStatus.Running ? true : false;
+            bool expectedValue = false;
 
             Assert.AreEqual(actualValue, expectedValue);
         }
